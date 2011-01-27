109b92926452ec36d40e4397d559f7157715468d
Volker Dusch <github@wallbash.com>
2011-01-27 16:58:46 +0100

    testing stuff

---
+++ source/String-COPY-for-testing.php
@@ -0,0 +1,64 @@
+<?php
+
+class String implements Countable, ArrayAccess {
+
+    protected $string;
+
+    public function __construct($string) {
+        $this->string = (string)$string;
+    }
+
+    public function __toString() {
+        return $this->string;
+    }
+
+    public function copy() {
+        return clone $this;
+    }
+
+    public function length() {
+        return strlen($this->string);
+    }
+
+    public function count() {
+        return $this->length();
+    }
+
+    public function toLowerCase() {
+        $this->string = strtolower($this->string);
+        return $this;
+    }
+
+    public function toUpperCase() {
+        $this->string = strtoupper($this->string);
+        return $this;
+    }
+
+
+    /* Implement ArrayAccess */
+    public function offsetExists($offset) {
+        if(isset($this->string[$offset])) {
+            return true;
+        }
+        return false;
+    }
+
+    public function offsetGet($offset) {
+        if(isset($this->string[$offset])) {
+            return $this->string[$offset];
+        }
+        throw new OutOfBoundsException("Uninitialized string offset: $offset");
+    }
+
+    public function offsetSet($offset, $value) {
+        $this->string[$offset] = $value;
+    }
+
+    public function offsetUnset($offset) {
+        throw new Exception("Cannot unset string offsets");
+    }
+
+    /* /Implement ArrayAccess */
+
+}
+
